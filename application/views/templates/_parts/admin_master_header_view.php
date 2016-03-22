ad6d9d57d0a01d00fea98e8a47a47f69a253f01b
Nor Khakim <nor.khakim@gmail.com>
2016-03-22 15:09:24 +0700

    -- ilangin muneu maps

--- application/views/templates/_parts/admin_master_header_view.php
+++ application/views/templates/_parts/admin_master_header_view.php
@@ -196,7 +196,7 @@ if($this->ion_auth->logged_in()) {
 						<li class="" id="menuPendaftaran"><a href="<?php echo site_url('admin/pendaftaran');?>"><i class="icon-double-angle-right "></i> Pendaftaran</a> </li>
 						<li class="" id="menuAlat"><a href="<?php echo site_url('admin/alat');?>"><i class="icon-double-angle-right "></i> Alat</a></li>
 						<li class="" id="menuPetugas"><a href="<?php echo site_url('admin/petugas');?>"><i class="icon-double-angle-right "></i> Petugas</a></li>
-						<li class="" id="menuMaps"><a href="<?php echo site_url('admin/maps');?>"><i class="icon-double-angle-right "></i> Maps</a></span></li>
+						<!-- <li class="" id="menuMaps"><a href="<?php echo site_url('admin/maps');?>"><i class="icon-double-angle-right "></i> Maps</a></span></li>-->
 					</ul>
                 </li>
 
